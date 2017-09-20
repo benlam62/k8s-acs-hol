9d93b5feaee2b2d1fae18f0702ad8a71c318146b
benlam62 <benlam62@gmail.com>
2017-09-20 15:30:56 +0800

    '170920'

--- calculator-api/src/main/java/ms/kenchen/Calculator/CalculatorService.java
+++ calculator-api/src/main/java/ms/kenchen/Calculator/CalculatorService.java
@@ -14,7 +14,7 @@ public class CalculatorService {
     @Path("ping")
     @Produces(MediaType.TEXT_PLAIN)
     public String ping() {
-        return "Welcome to Java Web API on ACS v8!\n" + new Date().toString();
+        return "Welcome to Java Web API on ACS v9!\n" + new Date().toString();
     }
 
     @GET
